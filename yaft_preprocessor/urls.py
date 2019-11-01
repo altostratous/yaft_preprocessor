b90e2535b9b9b52539a3c84d4c5dee613c5445f6
ce442-971-94105377 <altostratous@gmail.com>
2019-11-01 17:18:50 +0330

    Add url for preprocessor

--- yaft_preprocessor/urls.py
+++ yaft_preprocessor/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from yaft_preprocessor.views import PreprocessView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/preprocess_documents/', PreprocessView.as_view())
 ]
