27cd986ddaf0adcf2ee63b05a0825f3549ff310b
ce442-971-94105377 <altostratous@gmail.com>
2019-12-20 18:56:34 +0330

    Fix task discovery

--- yaft_preprocessor/celery.py
+++ yaft_preprocessor/celery.py
@@ -1,14 +1,6 @@
-from celery import Celery, shared_task
-from django.core.cache import caches, cache
-
-from yaft_preprocessor.utils.classification import Classifier
+from celery import Celery
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 
-@app.task
-def train(key, method, param):
-    classifier = Classifier(method, param)
-    classifier.train()
-    caches['classification'].set(key, classifier)
-    cache.set('classification_is_under_process', False)
+app.autodiscover_tasks()
---
+++ yaft_preprocessor/tasks.py
@@ -0,0 +1,12 @@
+from celery import shared_task
+from django.core.cache import caches, cache
+
+from yaft_preprocessor.utils.classification import Classifier
+
+
+@shared_task
+def train(key, method, param):
+    classifier = Classifier(method, param)
+    classifier.train()
+    caches['classification'].set(key, classifier)
+    cache.set('classification_is_under_process', False)
