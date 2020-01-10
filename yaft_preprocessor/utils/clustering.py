85c7d0f38491aa9616886b0eca48a5b2daac2037
ce442-971-94105377 <altostratous@gmail.com>
2020-01-10 22:03:20 +0330

    Fix getting dimension

--- yaft_preprocessor/utils/clustering.py
+++ yaft_preprocessor/utils/clustering.py
@@ -12,7 +12,7 @@ CLUSTERING_METHODS = {
 
 def cluster(documents, classifier_slug, k):
     ids, vectors = zip(*[(d['id'], d['vector']) for d in documents])
-    n = max(max(map(int, vector.keys())) + 1 for vector in vectors)
+    n = max(max(map(int, vector.keys() or [-1])) + 1 for vector in vectors)
     vectors = [expend_vector_of_size(vector, n) for vector in vectors]
     return dict(zip(
         ids, CLUSTERING_METHODS[classifier_slug](k).fit_predict(vectors)
