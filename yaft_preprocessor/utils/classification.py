f71f5a4af9daf328afe3e78708471de97fae84fb
Mohammad Haghighat <mhaghighat20@gmail.com>
2020-01-10 18:22:56 +0330

    Pre Adding 3 Clustering algorithms

--- yaft_preprocessor/utils/classification.py
+++ yaft_preprocessor/utils/classification.py
@@ -4,6 +4,10 @@ from typing import Any
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier as SKRandomForestClassifier
 
+from sklearn.cluster import KMeans
+from sklearn import mixture
+from sklearn.cluster import AgglomerativeClustering
+
 from django.core.cache import caches
 
 
@@ -27,6 +31,12 @@ class Classifier:
             return NaiveBayesClassifier(method, param)
         if method == 'knn':
             return KNNClassifier(method, param)
+        if method == 'kmeans':
+            return KMeansClustering(method, param)
+        if method == 'gmm':
+            return GMMClustering(method, param)
+        if method == 'hierarchical':
+            return HierarchicalClustering(method, param)
 
     def __init__(self, method: str, param: float) -> None:
         self.method = method
@@ -95,3 +105,44 @@ class RandomForestClassifier(Classifier):
 
     def classify_document(self, document) -> int:
         return self.classifier.predict([self.expand_vector(document)])[0]
+
+
+class KMeansClustering(Clusterer):
+    def train_using_training_set(self, x, y):
+        self.classifier.fit(x, y)
+
+    def __init__(self, method: str, param: float) -> None:
+        super().__init__(method, param)
+        # https://scikit-learn.org/stable/auto_examples/text/plot_document_clustering.html#sphx-glr-auto-examples-text-plot-document-clustering-py
+        self.classifier = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1,
+                verbose=opts.verbose)
+
+    def classify_document(self, document) -> int:
+        return self.classifier.predict([self.expand_vector(document)])[0]
+
+
+class GMMClustering(Clusterer):
+    def train_using_training_set(self, x, y):
+        self.classifier.fit(x, y)
+
+    def __init__(self, method: str, param: float) -> None:
+        super().__init__(method, param)
+        # https://scikit-learn.org/stable/auto_examples/mixture/plot_gmm_selection.html#sphx-glr-auto-examples-mixture-plot-gmm-selection-py
+        self.classifier = mixture.GaussianMixture(n_components=5, covariance_type='spherical')
+
+    def classify_document(self, document) -> int:
+        return self.classifier.predict([self.expand_vector(document)])[0]
+
+
+class HierarchicalClustering(Clusterer):
+    def train_using_training_set(self, x, y):
+        self.classifier.fit(x, y)
+
+    def __init__(self, method: str, param: float) -> None:
+        super().__init__(method, param)
+        # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html#sklearn.cluster.AgglomerativeClustering
+        self.classifier = AgglomerativeClustering()
+
+    def classify_document(self, document) -> int:
+        return self.classifier.predict([self.expand_vector(document)])[0]
+
